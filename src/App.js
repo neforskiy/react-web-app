ea538a1e92ea0ad04acc52a191fb05ba64ea6c66
Кальченко Тимофей <hackerbro228007@mail.ru>
2024-08-30 13:12:24 +0300

    nothiing

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { useEffect } from 'react';
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+
+
+
+function App() {
+    const {onToggleButton, tg} = useTelegram();
+
+    useEffect(() => {
+      tg.ready()
+    }, [])
+
+
+
+  return (
+    <div className="App">
+        <Header/>
+        <button onClick={onToggleButton}>toggle</button>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Form.css'
+const Form = () => {
+    return (
+        <div>
+            FORM
+        </div>
+    );
+};
+
+export default Form;
