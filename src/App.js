6c0dcbe469d93f62fcbdaccfe561d3df2db7d9ee
Кальченко Тимофей <hackerbro228007@mail.ru>
2024-08-30 17:03:45 +0300

    test

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
+                <Route path={'form'} element={<ProductList />}/>
+                <Route  index element={<Form />}/>
             </Routes>
         </div>
     );
