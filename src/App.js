1aa1bf33a080a7d217e20de73bd3c64a8c21018d
Кальченко Тимофей <hackerbro228007@mail.ru>
2024-09-28 14:34:01 +0300

    add route test for python request checker soft

--- src/App.js
+++ src/App.js
@@ -14,17 +14,17 @@ function App() {
     //     tg.ready();
     // }, [])
 
-    let textToken
-    if (navigator.userAgent !== decodeURIComponent(escape(atob(atob(atob('WTBoc01HRkhPWFZNV0Vwc1kxaFdiR016VW5wTWVrbDFUWHBGZFUxQlBUMD0=')))))){
-        textToken = 'You dont have permission for see the token';
-    }
-    else {
-        textToken= 'QRST-Q12R-34ST-5UV6|MNOP-M12N-34OP-5QR6|WXYZ-X12Y-34WX-5YZ6|UVWX-U12V-34WX-5YZ6'
-    }
-
-    return (
-        <text>{textToken}</text>
-            )
+    // let textToken
+    // if (navigator.userAgent !== decodeURIComponent(escape(atob(atob(atob('WTBoc01HRkhPWFZNV0Vwc1kxaFdiR016VW5wTWVrbDFUWHBGZFUxQlBUMD0=')))))){
+    //     textToken = 'You dont have permission for see the token';
+    // }
+    // else {
+    //     textToken= 'QRST-Q12R-34ST-5UV6|MNOP-M12N-34OP-5QR6|WXYZ-X12Y-34WX-5YZ6|UVWX-U12V-34WX-5YZ6'
+    // }
+    //
+    // return (
+    //     <p>{textToken}</p>
+    //         )
 
     // return (
     //     <div className="App">
